80a8dfac23daa95eb1d37371f1262a39033642e0
AnkushRawat0 <ankushrawat004@gmail.com>
2025-08-26 21:20:42 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,14 @@ app.use("/api/auth", authRoutes);
 
 
 //error handler middleware 
-
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode == 200 ? 500 :res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message , 
+        stack:process.env.NODE_ENV === "production" ? null :err.stack,
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
