1e0e47d977e02d66fb6771ecceb831f794b41b49
AnkushRawat0 <ankushrawat004@gmail.com>
2025-09-02 22:10:41 +0530

    cahnged gitignore

--- frontend/src/utils/axiosInstance.tsx
+++ frontend/src/utils/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL:'http://localhost:5000/api', 
   withCredentials: true, // Crucial for sending cookies (httpOnly refresh token)
 });
 
