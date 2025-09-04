cfd16a77cb59d01b64883905fdb61cd66376d22f
AnkushRawat0 <ankushrawat004@gmail.com>
2025-09-04 13:56:23 +0530

    backend deployed link added

--- frontend/src/utils/axiosInstance.tsx
+++ frontend/src/utils/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new Axios instance
 const axiosInstance = axios.create({
-  baseURL:'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_BASE, 
   withCredentials: true, // Crucial for sending cookies (httpOnly refresh token)
 });
 
